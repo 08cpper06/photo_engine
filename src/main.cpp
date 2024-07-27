6c0d7ba6600efb86229ceb84b9eb52aaa226f2a0
ryuse- <08cpper06@gmail.com>
2024-07-27 20:58:16 +0900

    - glfw3 HelloWorld

--- src/main.cpp
+++ src/main.cpp
@@ -1,6 +1,33 @@
 #include <iostream>
+#include <GLFW/glfw3.h>
+
+
+void error_callback(int error, const char* desc) {
+	std::cout << "E" << error << ": " << desc << std::endl;
+}
+
 
 int main() {
-	std::cout << "HelloWorld" << std::endl;
+	glfwSetErrorCallback(error_callback);
+
+	if (!glfwInit()) {
+		return 1;
+	}
+
+	GLFWwindow* window;
+	window = glfwCreateWindow(600, 400, "Preview", nullptr, nullptr);
+	if (!window) {
+		glfwTerminate();
+	}
+
+	glfwMakeContextCurrent(window);
+
+	while (!glfwWindowShouldClose(window)) {
+		glfwPollEvents();
+	}
+	glfwDestroyWindow(window);
+
+	glfwTerminate();
+
 	return 0;
 }
